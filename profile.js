aba7afb783998cdf45eef79b95a43d8cdd2f6a53
minionovdev <klucnikpetr9@gmail.com>
2026-04-14 20:45:05 +0300

    Update profile.js

--- profile.js
+++ profile.js
@@ -4,7 +4,20 @@ document.getElementById("name").innerText =
 document.getElementById("score").innerText =
     localStorage.getItem("score") || 0;
 
-function setSkin(skin){
-    localStorage.setItem("coinSkin", skin);
-    alert("Скин применён!");
-}
+let container = document.getElementById("achievements");
+let done = JSON.parse(localStorage.getItem("achievements")) || [];
+
+let list = [
+    "67 кликов",
+    "228 кликов",
+    "1 млн",
+    "10 млн",
+    "1 млрд"
+];
+
+list.forEach((t,i)=>{
+    let div=document.createElement("div");
+    div.className="card";
+    div.innerText = (done.includes(i+1) ? "✅ " : "❌ ") + t;
+    container.appendChild(div);
+});
